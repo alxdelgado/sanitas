b7b7fafd4620d9a88d96bf5d4c2f888ff4b09579
Alex Delgado <delgadoalex@icloud.com>
2020-07-25 09:29:01 -0500

    added line graph

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'; 
 import { Line } from 'react-chartjs-2';
+import numeral from 'numeral';
 
 const options = {
     legend: {
@@ -20,47 +21,76 @@ const options = {
             },
         },
     },
+    scales: {
+        xAxes: [
+            {
+                type: "time", 
+                time: {
+                    format: "MM/DD/YY", 
+                    tooltipFormat: "ll", 
+                }, 
+            }, 
+        ], 
+        yAxes: [
+            {
+                gridLines: {
+                    display: false,
+                },
+                ticks: {
+                    callback: function (value, index, values) {
+                        return numeral(value).format("0a");
+                    },
+                },
+            },
+        ],
+    }
 }; 
+
+
+const buildChartData = (data, caseType = "cases") => {
+    const chartData = []; 
+    let lastDataPoint; 
+
+    for (let date in data.cases) {
+        if (lastDataPoint) {
+            let newDataPoint = {
+                x: date, 
+                y: data[caseType][date] - lastDataPoint
+            }
+            chartData.push(newDataPoint); 
+        }
+        lastDataPoint = data[caseType][date];
+    }
+    return chartData;
+}
 
 function LineGraph() {
     const [data, setData] = useState({});
 
-
-    const buildChartData = (data, caseType = "cases") => {
-        const chartData = []; 
-        let lastDataPoint; 
-
-        for (let date in data.cases) {
-            if (lastDataPoint) {
-                let newDataPoint = {
-                    x: date, 
-                    y: data[caseType][date] - lastDataPoint
-                }
-                chartData.push(newDataPoint); 
-            }
-            lastDataPoint = data[caseType][date];
-        }
-    }
-
     useEffect(() => {
-        fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
-        .then(response => response.json())
-        .then(data => {
-            console.log('Line Graph >>> ', data);
+        const fetchData = async () => {
+            await fetch('https://disease.sh/v3/covid-19/historical/all?lastdays=120')
+            .then(response => response.json())
+            .then(data => {
+                console.log('Line Graph >>> ', data);
 
-            let chartData = buildChartData(data); 
-            setData(chartData);
-
-        })
-
+                let chartData = buildChartData(data, "cases"); 
+                setData(chartData);
+            }); 
+        }
+        fetchData(); 
     }, []); 
 
+    // I'm using optional chaining in the condition to return the linegraph component. >>> {data?.length etc...} <<< 
+    // could also be written as so: {data && data.length etc...}
     return (
         <div>
-            <Line 
-                data={{ datasets: [{ data: data, backgroundColor: 'rgba(204, 16, 52, 0.5)', borderColor: '#CC1034' }]}}
-                options={options}
-            />
+            {data?.length > 0 && (
+                <Line 
+                    data={{ datasets: [{ data: data, backgroundColor: 'rgba(204, 16, 52, 0.5)', borderColor: '#CC1034' }]}}
+                    options={options}
+                />
+            )}
         </div>
     )
 }
